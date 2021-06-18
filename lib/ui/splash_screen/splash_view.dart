ea8e79d6f6d32b363b62de845bdee0d93eef4185
Shreeyash Haritashya <haritashya@gmail.com>
2021-06-18 11:59:41 +0530

    feat: splash screen auto navigation added

--- lib/ui/splash_screen/splash_view.dart
+++ lib/ui/splash_screen/splash_view.dart
@@ -1,7 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:wiki_search/app/app.locator.dart';
+import 'package:wiki_search/app/app.router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final navigation = locator<NavigationService>();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      // 5s over, navigate to a new page
+      navigation.replaceWith(Routes.startUpView);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
