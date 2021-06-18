26fcea8f79b6cc6cd9c4206de5e0a20bc591dbd8
Shreeyash Haritashya <haritashya@gmail.com>
2021-06-18 11:53:53 +0530

    feat: splash screen ui completed

--- lib/ui/splash_screen/splash_view.dart
+++ lib/ui/splash_screen/splash_view.dart
@@ -5,9 +5,23 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Image.asset("assets/wikiLogo.png"),
+          Text(
+            'Wiki Search',
+            style: TextStyle(
+                color: Colors.grey[700], decoration: TextDecoration.none),
+          ),
+          Text(
+            'Shreeyash Hatitashya',
+            style: TextStyle(
+                color: Colors.grey[700],
+                decoration: TextDecoration.none,
+                fontSize: 24),
+          ),
         ],
       ),
     );
